1224012787bcf4dbb3af306e8a1a79160a90ab6f
liqiongchao-home <liqiongchao2016@gmail.com>
2020-04-18 21:24:54 +0800

    编程式路由导航

--- src/pages/Message.vue
+++ src/pages/Message.vue
@@ -1,10 +1,14 @@
 <template>
   <div>
-    <ul v-for="(msg, index) in messageArr" :key="msg.id">
-      <li><router-link :to="`/home/message/detail/${msg.id}`">{{msg.title}}</router-link></li>
+    <ul v-for="msg in messageArr" :key="msg.id">
+      <li>
+        <router-link :to="`/home/message/detail/${msg.id}`">{{msg.title}}</router-link>
+        <button @click="pushShow(msg.id)">push查看</button>
+        <button @click="replaceShow(msg.id)">replace查看</button>
+      </li>
     </ul>
-
     <hr>
+    <button @click="$router.back()">回退</button>
     <router-view></router-view>
   </div>
 </template>
@@ -27,6 +31,15 @@
         ]
         this.messageArr = msgs
       }, 1000)
+    },
+
+    methods: {
+      pushShow(id) {
+        this.$router.push(`/home/message/detail/${id}`);
+      },
+      replaceShow(id) {
+        this.$router.replace(`/home/message/detail/${id}`)
+      }
     }
   }
 </script>
