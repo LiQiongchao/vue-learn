8113a300a717fd036131a189a537f5352e5dbb18
liqiongchao-home <liqiongchao2016@gmail.com>
2020-05-04 23:03:06 +0800

    vuex实现counter-V1备份

---
+++ src/App-vuex-bak1.vue
@@ -0,0 +1,49 @@
+<template>
+  <div>
+    <p>clicked : {{$store.state.count}} times, count is {{isEvenOrOdd}}</p>
+    <button @click="increment">+</button>
+    <button @click="decrement">-</button>
+    <button @click="incrementIfOdd">increment if odd</button>
+    <button @click="incrementAsync">increment async</button>
+  </div>
+</template>
+
+
+<script>
+    export default {
+      name: "App",
+      data() {
+          return{
+            count: 0
+          }
+      },
+
+      computed: {
+        isEvenOrOdd() {
+          return this.$store.getters.isEvenOrOdd
+        }
+      },
+      methods: {
+        // 加1操作
+        increment() {
+          this.$store.dispatch('increment')
+        },
+        //减1操作
+        decrement() {
+          this.$store.dispatch('decrement')
+        },
+        // 如果是奇数就加1
+        incrementIfOdd() {
+          this.$store.dispatch('incrementIfOdd')
+        },
+        // 异步加1
+        incrementAsync() {
+          this.$store.dispatch('incrementAsync')
+        }
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
